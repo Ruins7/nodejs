92fa08ccd1ff2df01f34c71bf05fe7b28231b64b
Ruins7 <704657512@qq.com>
2017-02-28 22:25:46 -0500

    lesson15

---
+++ lesson15/main.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/nodejslesson15');
+
+var Cat = mongoose.model('Cat', {
+     name:String,
+     friends: [String],
+     age:Number,
+});
+
+var kitty = new Cat({name:'yangyang',friends:['Tom','Jerry']});
+kitty.age = 3;
+
+kitty.save(function(err){
+   if (err){
+     console.log('error...');
+   }
+	console.log('meow');
+});
